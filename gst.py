ba27ab469f9da074a73c979fdd4e53b6c54a130d
Nupur Banakar <78011036+Nupurbanakar@users.noreply.github.com>
2024-01-16 12:14:23 +0530

    Update gst.py

--- gst.py
+++ gst.py
@@ -1,3 +1,9 @@
+class Item:
+    def __init__(self, units, item_type, unit_price):
+        self.units = units
+        self.item_type = item_type
+        self.unit_price = unit_price
+
 class GSTCalculator:
     def __init__(self):
         self.gst_rates = {
@@ -5,15 +11,10 @@ class GSTCalculator:
             "Furniture": 5,
             "Electronics": 18,
             "Cosmetics": 28
-                         }
+        }
         self.items = []
 
-    def add_item(self, units, item_type, unit_price):
-        item = {
-            "units": units,
-            "item_type": item_type,
-            "unit_price": unit_price
-               }
+    def add_item(self, item):
         self.items.append(item)
 
     def calculate_total_gst(self):
@@ -21,17 +22,14 @@ class GSTCalculator:
         total_final_price = 0
 
         for item in self.items:
-            units = item["units"]
-            item_type = item["item_type"]
-            unit_price = item["unit_price"]
-            gst_rate = self.gst_rates.get(item_type, 0)
-            net_gst_per_unit = (gst_rate / 100) * unit_price
-            final_price_per_unit = unit_price + net_gst_per_unit
+            gst_rate = self.gst_rates.get(item.item_type, 0)
+            net_gst_per_unit = (gst_rate / 100) * item.unit_price
+            final_price_per_unit = item.unit_price + net_gst_per_unit
 
-            total_net_gst += net_gst_per_unit * units
-            total_final_price += final_price_per_unit * units
+            total_net_gst += net_gst_per_unit * item.units
+            total_final_price += final_price_per_unit * item.units
 
-            print(f"{item_type}: Net GST per unit - {net_gst_per_unit}, Final selling price - {final_price_per_unit}")
+            print(f"{item.item_type}: Net GST per unit - {net_gst_per_unit}, Final selling price - {final_price_per_unit}")
 
         return round(total_net_gst, 2), round(total_final_price, 2)
 
@@ -48,7 +46,8 @@ def main():
             item_type = input("Enter the type of item: ").capitalize()
             unit_price = float(input("Enter the initial unit price: "))
 
-            gst_calculator.add_item(units, item_type, unit_price)
+            item = Item(units, item_type, unit_price)
+            gst_calculator.add_item(item)
 
         except ValueError:
             print("Invalid input. Please enter valid numeric values.")
