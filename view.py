383b839614b3343895c484f7729247d7804d956c
Nupur Banakar <78011036+Nupurbanakar@users.noreply.github.com>
2024-01-18 14:38:34 +0530

    Delete view.py

--- view.py
+++
@@ -1,9 +0,0 @@
-class GSTView:
-    @staticmethod
-    def display_result(net_gst, final_price, details):
-        print("\nDetails:")
-        for detail in details:
-            print(detail)
-
-        print(f"\nTotal Net GST amount: {net_gst}")
-        print(f"Total Final selling price: {final_price}")
